d6931af8cb974f7190a3f922fd76d03fdf7e9e55
Rajgopal Kumar <rajgk0000@gmail.com>
2024-01-05 17:10:00 +0530

    add graph to explore

---
+++ lib/section/Explore_section.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class Explore_section extends StatefulWidget {
+  const Explore_section({super.key});
+
+  @override
+  State<Explore_section> createState() => _Explore_sectionState();
+}
+
+class _Explore_sectionState extends State<Explore_section> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 24, left: 16, right: 16),
+      child: Column(
+        children: [
+          InkWell(
+            onTap: () {},
+            child: Container(
+              padding: EdgeInsets.only(left: 8),
+              child: Row(
+                children: [
+                  Text(
+                    'ITC',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                        color: Color.fromRGBO(4, 23, 39, 1)),
+                  ),
+                  SizedBox(
+                    width: 128,
+                  ),
+                  Text(
+                    'ITC',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Color.fromRGBO(4, 23, 39, 1)),
+                  ),
+                  SizedBox(
+                    width: 56,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        '₹455.20',
+                        style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                            color: Color.fromRGBO(4, 23, 39, 1)),
+                      ),
+                      Text(
+                        '+3.75(0.83%)',
+                        style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                            color: Color.fromRGBO(18, 209, 142, 1)),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
