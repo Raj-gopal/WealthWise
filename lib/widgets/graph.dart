ce9d66d6bccd0490ae4a5b3e56e63337957eefdd
Rajgopal Kumar <rajgk0000@gmail.com>
2024-01-17 23:24:35 +0530

    add line graph

--- lib/widgets/graph.dart
+++ lib/widgets/graph.dart
@@ -1,57 +1,64 @@
 import 'dart:convert';
-
 import 'package:chart_sparkline/chart_sparkline.dart';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:wealthwise/model/Intraday.dart';
-import 'package:wealthwise/model/company_name.dart';
-import 'package:wealthwise/widgets/user_data.dart';
+import 'package:flutter/material.dart';
 
-class graph_section extends StatefulWidget {
-  graph_section({Key? key}) : super(key: key);
+import 'package:wealthwise/model/Intraday.dart';
+
+class CandlestickGraph extends StatefulWidget {
+  const CandlestickGraph({Key? key}) : super(key: key);
 
   @override
-  State<graph_section> createState() => _graph_sectionState();
+  _CandlestickGraphState createState() => _CandlestickGraphState();
 }
 
-class _graph_sectionState extends State<graph_section> {
-  List<GraphStockApi> graphstockApi = [];
-
+class _CandlestickGraphState extends State<CandlestickGraph> {
   @override
   Widget build(BuildContext context) {
+    return Expanded(
+      child: FutureBuilder(
+                    future: getdata(),
+                    builder: (context, AsyncSnapshot<GraphStockApi> snapshot) {
+                      if (!snapshot.hasData) {
+                        return Container(
+                          alignment: Alignment.center,
+                            height: 80,
+                            width: 80,
+                            child: CircularProgressIndicator(
+                              color: Color.fromRGBO(3, 127, 255, 1),
+                            ));
+                      } else {
+                        var sparklineData = snapshot.data!.results
+                            .map<double>(
+                                (result) => double.parse(result.vw.toString()))
+                            .toList();
     
-    return FutureBuilder(
-      future: getdata(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return ListView.builder(
-            itemCount: snapshot.data?.results.length,
-            itemBuilder: (context, index) {
-              return Container(
-                width: 32,
-                height: 56,
-                child: Sparkline(
-                 data: [1,2,-6,-1,5],
-                  ),
-                );          
-            },
-          );
-        } else if (snapshot.hasError) {
-          return Center(
-            child: Text('Error: ${snapshot.error}'),
-          );
-        } else {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      },
+                        return Container(
+                            child: Sparkline(
+                          data: sparklineData,
+                          lineWidth: 2,
+                          lineColor: Color.fromRGBO(18, 209, 142, 1),
+                          fillMode: FillMode.below,
+                          fillGradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Color.fromRGBO(18, 209, 142, 1),
+                              Colors.white10
+                            ],
+                          ),
+                          averageLine: true,
+                          averageLabel: true,
+                        ));
+                      }
+                    }),
     );
+    
   }
 
-  Future<GraphStockApi> getdata() async {
+    Future<GraphStockApi> getdata() async {
     final response = await http.get(Uri.parse(
-        'https://api.polygon.io/v2/aggs/ticker/VNLA/range/1/minute/2023-01-11/2023-01-11?sort=desc&limit=10&apiKey=h8gjI2GQTJ1KibD7oZXacUGOhTS5qKKq'));
+        'https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/minute/2024-01-12/2024-01-12?sort=desc&limit=50&apiKey=h8gjI2GQTJ1KibD7oZXacUGOhTS5qKKq'));
     var data = jsonDecode(response.body.toString());
 
     if (response.statusCode == 200) {
@@ -60,4 +67,4 @@ class _graph_sectionState extends State<graph_section> {
       return GraphStockApi.fromJson(data);
     }
   }
-}
+  }
