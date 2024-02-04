acd185c2471bf157128d081aca1393fea26a26df
Rajgopal Kumar <rajgk0000@gmail.com>
2024-02-05 03:34:10 +0530

    add Fund page done

--- lib/screen/Add_Fund.dart
+++ lib/screen/Add_Fund.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:auto_size_text_field/auto_size_text_field.dart';
 
 class Add_Fund extends StatelessWidget {
   const Add_Fund({super.key});
@@ -11,12 +12,13 @@ class Add_Fund extends StatelessWidget {
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.only(
-          top: 48,
+          top: 48, left: 24, right: 24
         ),
-        child: Column(
-          children: [
-            Center(
-              child: Text(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
                 'Add Money',
                 style: TextStyle(
                   color: Color(0xFF041727),
@@ -25,12 +27,10 @@ class Add_Fund extends StatelessWidget {
                   fontWeight: FontWeight.w900,
                 ),
               ),
-            ),
-            Center(
-              child: Padding(
+              Padding(
                 padding: const EdgeInsets.only(top: 4),
                 child: Text(
-                  '\$400',
+                  '\$400 available',
                   style: TextStyle(
                     fontFamily: 'Montserrat',
                     fontSize: 16,
@@ -39,49 +39,83 @@ class Add_Fund extends StatelessWidget {
                   ),
                 ),
               ),
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 32),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      '\$',
-                      style: TextStyle(
-                        color: Color(0xFF041727),
-                        fontFamily: 'Montserrat',
-                        fontSize: 32,
-                        fontWeight: FontWeight.w900,
-                      ),
+              Padding(
+                padding: const EdgeInsets.only(top: 32, right: 32),
+                child: AutoSizeTextField(
+                  controller: money,
+                  fullwidth: false,
+                  // minWidth: 100,
+                  //   textAlignVertical: TextAlignVertical.center,
+                  style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 32,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.black),
+                  cursorColor: Color.fromRGBO(196, 196, 197, 1),
+                  cursorHeight: 32,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText: '\$',
+                    prefixStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 32,
+                      fontWeight: FontWeight.w900,
+                      color: Color.fromRGBO(91, 91, 91, 1),
                     ),
-                    SizedBox(
-                      width: 32,
-                      child: TextField(
-                        controller: money,
-                        cursorColor:  Color.fromRGBO(196, 196, 197, 1),
-                        cursorHeight: 32,
-                        keyboardType: TextInputType.number,
-                        decoration: const InputDecoration(
-                          hintText: '0',
-                          hoverColor: Color.fromRGBO(91, 91, 91, 1),
-                          focusColor: Color(0xFF041727),
-                          hintStyle: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 32,
-                            fontWeight: FontWeight.w900,
-                          ),
-                          
-                           border: InputBorder.none,
-                        ),
-                      ),
+                    hintText: '0',
+                    hoverColor: Color.fromRGBO(91, 91, 91, 1),
+                    focusColor: Color(0xFF041727),
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 32,
+                      fontWeight: FontWeight.w900,
+                      color: Color.fromRGBO(91, 91, 91, 1),
                     ),
-                  ],
+                    border: InputBorder.none,
+                  ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 112,),
+
+              Container(
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(209, 18, 18, 1),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Center(
+                  child: Text('Amount should be greater than or equal to 100',
+                  style:  TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 12,
+                      fontWeight: FontWeight.w900,
+                      color: Color.fromRGBO(252, 178, 185, 1),
+                    ),),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(top: 24, left: 24, right: 24 ,bottom: 24),
+        child: Container(
+          height: 72,
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(3, 127, 255, 1),
+            borderRadius: BorderRadius.circular(16)
+          ),
+          child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'ADD FUND',
+                  style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 24,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white),
+                ),
+              ),
         ),
       ),
     );
